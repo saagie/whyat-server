c307c724949129b551b78e76b00c5fdf759e9fc4
Pierre Leresteux <pierre.leresteux@gmail.com>
2017-06-16 15:04:20 +0200

    feat: add CORS in controller

--- src/main/kotlin/io/saagie/whyat/controller/EventController.kt
+++ src/main/kotlin/io/saagie/whyat/controller/EventController.kt
@@ -17,11 +17,13 @@ package io.saagie.whyat.controller
 
 import io.saagie.whyat.domain.Event
 import io.saagie.whyat.service.EventService
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin
 class EventController(val eventService: EventService) {
 
     @PostMapping("/event")
